3d32ae45b343994827f466f5704d76d64bd415a0
diaoyudong <yudongdiao@gmail.com>
2021-05-16 17:45:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="http://img.netbian.com/file/2021/0317/4120bc42500eb9be6082f23b6071c1c5.jpg">
+    <h1>好美嗷</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
